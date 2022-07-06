71e41c79b0a7cd1b1e40b8a838c3401d8b65dfeb
woowahan <woowahan@woowahan-MacBookPro-67.local>
2022-07-06 17:32:58 +0900

    refactor : 이니셜 프로젝트에 MVC 패턴 적용

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import { globalRouter } from "./routers/global.js";
+
+const app = express();
+const PORT = 4000;
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger("dev"));
+app.use(cookieParser());
+
+app.use("/", globalRouter);
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT} 🚀`));
